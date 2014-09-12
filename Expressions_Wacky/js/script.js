909c1cb101603c221748464ad273deb345a9789d
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:46:55 -0500

    added comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,10 +2,12 @@
 //Sept. 11, 2014
 //Expressions_Wacky
 
+//different group the bride needs to invite
 var brideGuest = [132, 43, 12];
 var momGuest = parseInt(prompt("How many people does your mom want invite to the wedding?"));
 var fianceGuest = parseInt(prompt("How many people does your fiance want invite to the wedding?"));
 
+//adds up all the bride's guests from the array
 var totalBrideGuest = brideGuest[0] + brideGuest[1] + brideGuest[2];
 
 var guestList = totalBrideGuest + momGuest + fianceGuest;
