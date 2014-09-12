44c37b40b610b4983cfa26736584eb12c80e40f2
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:48:48 -0500

    Comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,6 +6,7 @@ var meals = parseInt(prompt("How many times a day do you feed your dogs?"));
 var dog1 = parseInt(prompt("How much food does the first dog get each meal?"));
 var dog2 = parseInt(prompt("How much food does the second dog get each meal?"));
 
+//adds the amount of food the dogs eat and then multiplies the amount by the meals they get every day
 var result = (dog1 + dog2) * meals;
 
 console.log("You will need " + result + " cups of food a day for your dogs.");
