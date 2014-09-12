991aab6e7f4aa7dd2d3b0942bee8d52202daa298
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:33:50 -0500

    Fixed wacky errors

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,11 +3,11 @@
 //Expressions_Wacky
 
 var guests = [132, 43, 12];
-var momGuest = parseInt(prompt("How many people does your mom want invite to the wedding?");
-var fianceGuest = parseInt(prompt("How many people does your fiance want invite to the wedding?");
+var momGuest = parseInt(prompt("How many people does your mom want invite to the wedding?"));
+var fianceGuest = parseInt(prompt("How many people does your fiance want invite to the wedding?"));
 
 var totalBrideGuest = guests[0] + guests[1] + guests[2];
 
-var guestList = total + momGuest + fianceGuest;
+var guestList = totalBrideGuest + momGuest + fianceGuest;
 
 console.log(guestList);
