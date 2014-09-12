8f912e3321f087d4b26594f30347e560fbcf2e77
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 21:53:19 -0500

    Added Expressions_Personal

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,13 @@
+//Callie Kavourgias
+//Sept. 11, 2014
+//Expressions_Personal
+
+var meals = parseInt(prompt("How many times a day do you feed your dogs?"));
+var dog1 = parseInt(prompt("How much food does the first dog get each meal?"));
+var dog2 = parseInt(prompt("How much food does the second dog get each meal?"));
+
+var result = (dog1 + dog2) * meals;
+
+console.log("You will need " + result + " cups of food a day for your dogs.");
+
+
