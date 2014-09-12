59902c909d2034b4a94ba614cc4b248abb9d81ff
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:05:08 -0500

    Industry first draft

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,4 +2,8 @@
 //Sept. 11, 2014
 //Expressions_Industry
 
+var images = parseInt(prompt("How many images do you have to display?");
+var horizontal = parseInt(prompt("How many images should display horizonally?"));
+var vertical = parseInt(prompt("How many images will display vertically?"))
 
+var result = horizontal * vertical 
