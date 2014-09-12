3ab9651408b4d4413f39f42c7e39277f54f0e734
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:33:07 -0500

    Updated Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,13 @@
 //Callie Kavourgias
 //Sept. 11, 2014
 //Expressions_Wacky
+
+var guests = [132, 43, 12];
+var momGuest = parseInt(prompt("How many people does your mom want invite to the wedding?");
+var fianceGuest = parseInt(prompt("How many people does your fiance want invite to the wedding?");
+
+var totalBrideGuest = guests[0] + guests[1] + guests[2];
+
+var guestList = total + momGuest + fianceGuest;
+
+console.log(guestList);
