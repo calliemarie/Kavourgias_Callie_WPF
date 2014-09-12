47a060e6c5e9e79f9ed2c2cf8664e033ac98532a
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:45:30 -0500

    Changed wording

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,6 +6,6 @@ var hours = parseInt(prompt("How many hours do you have on your sprint?"));
 var articleLength = parseInt(prompt("How many hours does it take you to write an article?"));
 var articleNeed = parseInt(prompt("How many articles do you need this sprint?"))
 
-var result = articleLength * articleNeed;
+var result = hours/ (articleLength * articleNeed);
 
 console.log("If you have " + hours + " hours in your sprint, then you will be able to finish " + result + " articles");
