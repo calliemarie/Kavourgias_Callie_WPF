98c0f7a15a7e24c1700ab59f4552ceebf58c033c
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:21:40 -0500

    New Result

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,10 +2,10 @@
 //Sept. 11, 2014
 //Expressions_Industry
 
-var hours = parseInt(prompt("How many hours do you have on your sprint");
-var articleLength = parseInt(prompt("How long does it take you to write an article?"));
+var hours = parseInt(prompt("How many hours do you have on your sprint?"));
+var articleLength = parseInt(prompt("How many hours does it take you to write an article?"));
 var articleNeed = parseInt(prompt("How many articles do you need this sprint?"))
 
 var result = articleLength * articleNeed;
 
-console.log(result);
+console.log("If you have " + hours + " hours in your sprint, then you will be able to finish " + result + " articles");
