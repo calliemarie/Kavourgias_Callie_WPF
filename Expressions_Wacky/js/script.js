20463277da82de15708893c690a0460f4364adca
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:38:09 -0500

    Changed variable name

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,11 +2,11 @@
 //Sept. 11, 2014
 //Expressions_Wacky
 
-var guests = [132, 43, 12];
+var brideGuest = [132, 43, 12];
 var momGuest = parseInt(prompt("How many people does your mom want invite to the wedding?"));
 var fianceGuest = parseInt(prompt("How many people does your fiance want invite to the wedding?"));
 
-var totalBrideGuest = guests[0] + guests[1] + guests[2];
+var totalBrideGuest = brideGuest[0] + brideGuest[1] + brideGuest[2];
 
 var guestList = totalBrideGuest + momGuest + fianceGuest;
 
