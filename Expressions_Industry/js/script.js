b4b7fdf337ea2ee280a750e2f145f736ff9770ab
Callie Kavourgias <calliemariek@gmail.com>
2014-09-11 22:14:15 -0500

    Sprint example

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,8 +2,10 @@
 //Sept. 11, 2014
 //Expressions_Industry
 
-var images = parseInt(prompt("How many images do you have to display?");
-var horizontal = parseInt(prompt("How many images should display horizonally?"));
-var vertical = parseInt(prompt("How many images will display vertically?"))
+var hours = parseInt(prompt("How many hours do you have on your sprint");
+var articleLength = parseInt(prompt("How long does it take you to write an article?"));
+var articleNeed = parseInt(prompt("How many articles do you need this sprint?"))
 
-var result = horizontal * vertical 
+var result = articleLength * articleNeed;
+
+console.log(result);
